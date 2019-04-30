5f9e8cdc600efdeb87d9084ddcd38320f6229bb9
Bryan Jeanty <bryan.jeanty@outlook.com>
2019-04-30 11:30:19 -0400

    nav bar added to app layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,43 @@
 import React from "react";
+import Link from "next/link";
+import { connect } from "react-redux";
 
-export const Layout = ({ children }) => {
-  return <div>{children}</div>;
+const Brand = ({ title, href }) => {
+  return (
+    <Link href={href}>
+      <a>
+        <h2>{title}</h2>
+      </a>
+    </Link>
+  );
 };
+
+const Layout = ({ children, user }) => {
+  return (
+    <div>
+      <nav>
+        <Brand href="/home" title="Music Search" />
+        <ul>
+          {!user ? (
+            <div>
+              <Link href="/signup">
+                <a>Sign Up</a>
+              </Link>
+              <Link href="/signin">
+                <a>Sign In</a>
+              </Link>
+            </div>
+          ) : (
+            <button type="submit">Sign Out</button>
+          )}
+        </ul>
+      </nav>
+      {children}
+    </div>
+  );
+};
+
+export default connect(
+  ({ user }) => ({ user }),
+  null
+)(Layout);
